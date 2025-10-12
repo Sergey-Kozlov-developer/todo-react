cffae052654ed1755df7e329dd14cc9dd51197a2
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2025-10-12 19:25:11 +0300

    fix work state checkbox & add useReducer

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -14,66 +14,69 @@ interface ITask {
 type ITaskList = Record<string, ITask>;
 type Action = { type: "TOGGLE_TASK"; taskId: string } | {type: "DELETE_TASK"; taskId: string };
 
-
-const TaskList = () => {
-    // начальное состояние
-    const initialMockTaskList: ITaskList = {
-        one: {
-            id: "1",
-            text: "Note #1",
-            completed: false,
-        },
-        two: {
-            id: "2",
-            text: "Note #2",
-            completed: true,
-        },
-        three: {
-            id: "3",
-            text: "Note #3",
-            completed: false,
-        },
-    };
-    // хук принимает в себя ф-цию состояния и начальное состояние
-    const [tasks, dispatch] = useReducer(taskReducer, initialMockTaskList);
+// начальное состояние
+const initialMockTaskList: ITaskList = {
+    one: {
+        id: "1",
+        text: "Note #1",
+        completed: false,
+    },
+    two: {
+        id: "2",
+        text: "Note #2",
+        completed: true,
+    },
+    three: {
+        id: "3",
+        text: "Note #3",
+        completed: false,
+    },
+};
 
 // ф-ция, которая меняет состояние задачи
-    function taskReducer(state: ITaskList, action: Action): ITaskList {
-        switch (action.type) {
-            case "TOGGLE_TASK":
-                return {
-                    ...state,
-                    [action.taskId]: {
-                        ...state[action.taskId],
-                        completed: !state[action.taskId].completed,
-                    },
-                };
+function taskReducer(state: ITaskList, action: Action): ITaskList {
+    switch (action.type) {
+        case "TOGGLE_TASK":
+            // создаем новый объект
+            // возвращаем новое состояние на основе предыдущего
+            // при dispatch определенного action
+            return {
+                ...state, // текущий state
+                [action.taskId]: {
+                    ...state[action.taskId],
+                    completed: !state[action.taskId].completed,
+                },
+            };
 
-            default:
-                return state;
-        }
+        default:
+            return state;
     }
+}
+
+const TaskList = () => {
+    // хук принимает в себя ф-цию состояния и начальное состояние
+    const [state, dispatch] = useReducer(taskReducer, initialMockTaskList);
     // ф-ция обрабатывает клик
     const toggleTaskCompletion = useCallback((taskId: string) => {
         dispatch({ type: "TOGGLE_TASK", taskId });
     }, []);
 
 
-    if (Object.keys(tasks).length === 0) {
+    if (Object.keys(state).length === 0) {
         return <EmptyBlock />;
     }
 
     return (
         <div className="task-list">
             <ul className="task-list__items">
-                {Object.keys(tasks).map((taskId) => (
+                {Object.keys(state).map((taskId) => (
                     <li key={taskId} className="task-list__item">
                         <div className="task-list__content">
                             <label className="task-list__checkbox-label">
                                 <input
                                     type="checkbox"
                                     className="task-list__checkbox"
-                                    checked={tasks[taskId].completed}
+                                    checked={state[taskId].completed}
                                     onChange={() =>
                                         toggleTaskCompletion(taskId)
                                     }
@@ -84,12 +87,12 @@ const TaskList = () => {
                             <div className="task-list__details">
                                 <span
                                     className={`task-list__text ${
-                                        tasks[taskId].completed
+                                        state[taskId].completed
                                             ? "task-list__text--completed"
                                             : ""
                                     }`}
                                 >
-                                    {tasks[taskId].text}
+                                    {state[taskId].text}
                                 </span>
                             </div>
                         </div>
